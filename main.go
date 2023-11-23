da6529797fd7ad40c445e8b003316348759828c5
Ryan Yang <ryanyang99@hotmail.com>
2023-11-23 11:07:01 -0800

    Update main.go

--- main.go
+++ main.go
@@ -2,28 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	robinhood "gitlab.com/brokerage-api/robinhood-client/client"
 )
 
 func main() {
-	if os.Getenv("ROBINHOOD_USERNAME") == "" {
-		return
-	}
-	o := &robinhood.CredsCacher{
-		Creds: &robinhood.OAuth{
-			Username: os.Getenv("ROBINHOOD_USERNAME"),
-			Password: os.Getenv("ROBINHOOD_PASSWORD"),
-		},
-	}
-	c, err := robinhood.Dial(&robinhood.CredsCacher{Creds: o})
-
-	//err
-	i, err := c.GetInstrumentForSymbol("SPY")
-	if err != nil {
-		fmt.Errorf("Unable to get SPY instrument")
-		return
-	}
-	fmt.Printf("%v", i)
+	fmt.Println("hello world")
 }
